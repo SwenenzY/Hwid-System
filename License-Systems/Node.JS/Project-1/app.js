87b40911b536194849ff5c5aad6543f8fbfdd731
Yiğit Kaan Bayat <44095096+SwenenzY@users.noreply.github.com>
2020-06-22 14:32:03 +0300

    First

---
+++ License-Systems/Node.JS/Project-1/app.js
@@ -0,0 +1,31 @@
+/*
+ * Author  : SwenenzY
+ * Website : https://swenenzy.com
+ * Repo    : https://github.com/editlater 
+ */
+const Http = require('http');
+const Express = require('express');
+const Fetch = require('node-superfetch');
+const App = Express();
+
+console.log("mrb"); 
+
+// Heroku Glitch etc. App.listen(process.env.PORT);
+App.listen(80); // 80 http for local  
+
+App.get("/", (request, response) => {
+	
+  response.send( 'Hoşgeldiniz efendim..' );
+  
+} ); 
+ 
+/*
+ * license for this project is manually defined
+ */
+App.get("/list", (Request, Response) => {
+
+	const Data = { Keys : [ "key1","key2" ] };
+	Response.json(Data);
+	console.error("Request!");
+} ); 
+ 
