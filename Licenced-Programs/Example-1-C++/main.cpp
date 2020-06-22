e3bc251224a4517ce75c5ca2c421662dd2235412
Yiğit Kaan Bayat <44095096+SwenenzY@users.noreply.github.com>
2020-06-22 14:58:19 +0300

    node.js Readme and edits
    node.js Readme and edits

---
+++ Licenced-Programs/Example-1-C++/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+
+	std::cout << "mrb" << std::endl;
+	return 0;
+
+}
--- License-Systems/Node.JS/Project-1/app.js
+++ License-Systems/Node.JS/Project-1/app.js
@@ -1,7 +1,7 @@
 /*
  * Author  : SwenenzY
  * Website : https://swenenzy.com
- * Repo    : https://github.com/editlater 
+ * Repo    : https://github.com/SwenenzY/Hwid-System/
  */
 const Http = require('http');
 const Express = require('express');
@@ -15,7 +15,7 @@ App.listen(80); // 80 http for local
 
 App.get("/", (request, response) => {
 	
-  response.send( 'Hoşgeldiniz efendim..' );
+  response.send( "Hoşgeldiniz efendim.." );
   
 } ); 
  
@@ -25,7 +25,8 @@ App.get("/", (request, response) => {
 App.get("/list", (Request, Response) => {
 
 	const Data = { Keys : [ "key1","key2" ] };
-	Response.json(Data);
-	console.error("Request!");
+	Response.json( Data );
+	console.error( "Request!" );
+	
 } ); 
  
