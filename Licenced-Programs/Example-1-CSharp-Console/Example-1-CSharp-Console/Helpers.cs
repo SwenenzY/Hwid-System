56ba35cd9f19a0080cbf6c0a7f4f983f087d0733
Yiğit Kaan Bayat <44095096+SwenenzY@users.noreply.github.com>
2020-06-22 16:15:06 +0300

    Csharp Upload & HTML Delete

---
+++ Licenced-Programs/Example-1-CSharp-Console/Example-1-CSharp-Console/Helpers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Management;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace Example_1_CSharp_Console
+{
+    static class Helpers
+    {
+        public static string GetCpuID()
+        {
+            var Mbs = new ManagementObjectSearcher("Select ProcessorId From Win32_processor"); // Search Processor
+            ManagementObjectCollection MbsList = Mbs.Get(); // Get list
+            foreach (ManagementObject Mo in MbsList) { return Mo["ProcessorId"].ToString(); } // if found ProcessorId return;
+            return null;
+        }
+        public static string GetWebResponse(string URL)
+        {
+            HttpWebRequest Request = WebRequest.Create(URL) as HttpWebRequest; // Creating Request
+            using (HttpWebResponse Response = Request.GetResponse() as HttpWebResponse) // Getting Response via web
+            {
+                StreamReader Reader = new StreamReader(Response.GetResponseStream()); // Reader for response
+                return Reader.ReadToEnd(); // reading all data and return
+            }
+        }
+        public static bool CheckLicense(string Data, string Key, string Element)
+            // Data = Web Response
+            // Key = Json segment
+            // Element = CPU ID etc.
+        {
+            try
+            {
+                JObject MyParser = JObject.Parse(Data); // Parse json with  Newtonsoft.Json
+                foreach (JToken ParsedElement in MyParser[Key]) // Searching ID
+                {
+                    if (ParsedElement.ToString() == Element)
+                    { // if ID == ID
+                        return true; // return true
+                    }
+                }
+                return false; // if cannot parse error etc.
+            }
+            catch (Exception)
+            {
+                return false; // if cannot parse error etc.
+            }
+        }
+    }
+}
---
+++ Licenced-Programs/Example-1-CSharp-Console/Example-1-CSharp-Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Example_1_CSharp_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Open Source ❤️ SwenenzY";
+            string WebResponseData = Helpers.GetWebResponse("http://127.0.0.1/list"); // Getting Response
+            string CpuID = Helpers.GetCpuID(); // Getting CPU ID
+            bool LicenseStatus = Helpers.CheckLicense(WebResponseData, "Keys", CpuID); // Checking License
+            Console.WriteLine(LicenseStatus ? "Have a license" : "Sorry :(" ); // ? : instead  of if else (Basicly)
+            Console.Read();
+        }
+    }
+}
